529435918fd7e585bf82c96609584f0d866c5491
Gi0vs <53981859+Gi0vs@users.noreply.github.com>
2021-10-07 15:25:53 -0400

    se arreglaron algunos desperfectos visuales

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,14 +3,14 @@ import React, { useEffect } from 'react';
 
 export default function MediaCard() {
 
-  
-    useEffect(() => {
-      window.scrollTo(0, 0);
-    }, []);
-  
-    return (
-        <>
-       
-        </>
-    );
-  }
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <div>
+      <Header item={'Home'} />
+    </div>
+  );
+}
