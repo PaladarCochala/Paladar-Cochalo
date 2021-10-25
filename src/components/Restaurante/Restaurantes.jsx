6da37031abece2d1b5ffbe721126be97f5b46952
Casazola67 <gabriel.casazola@ucb.edu.bo>
2021-10-24 20:17:07 -0500

    Paginacion Restaurantes

--- src/components/Restaurante/Restaurantes.jsx
+++ src/components/Restaurante/Restaurantes.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { getRestaurantes } from '../../services/restaurante';
 import { Grid, makeStyles } from '@material-ui/core';
 import RestaurantCard from '../Restaurante/Common/RestaurantCard';
+import ReactPaginate from "react-paginate";
+import "./css/Restaurante.css";
+
 
 const useStyles = makeStyles((theme) => ({
     container: {
@@ -17,12 +20,21 @@ const useStyles = makeStyles((theme) => ({
 export default function Restaurantes(props) {
 
     const classes = useStyles();
-    const [restaurantesBuscados, setRestaurantesBuscados] = useState([]);
+
+    const [restaurantes, setRestaurantes] = useState([]);
+
+    const [pageNumber, setPageNumber] = useState(0);
+    const restaurantesPerPage = 5;
+    const pagesVisited = pageNumber * restaurantesPerPage;
 
     useEffect(() => {
         getDataRestaurantes();
     }, [props]);
-
+/*
+    function filterByValue(array) {
+        return array.filter(o => o['nombre'].toLowerCase().includes(props.location.state.response.toLowerCase()));
+    }
+*/
     function getDataRestaurantes() {
         getRestaurantes()
             .then((response) => {
@@ -30,28 +42,45 @@ export default function Restaurantes(props) {
             })
             .then((response) => {
                 console.log(response.response)
-                setRestaurantesBuscados(filterByValue(response.response));
+                //setRestaurantes(filterByValue(response.response));
+                setRestaurantes(response.response);
             })
     }
 
-    function filterByValue(array) {
-        return array.filter(o => o['nombre'].toLowerCase().includes(props.location.state.response.toLowerCase()));
-    }
-    return (
-        <Grid container>
 
-            {restaurantesBuscados.map(restaurante => (
-                <>
-                    <Grid
-                        align="center"
-                        key={restaurante.id}
-                    // item xs={12} sm={3}
+    //LISTADO RESTAURANTES
+    const displayRestaurantes = restaurantes
+        .slice(pagesVisited, pagesVisited + restaurantesPerPage)
+        .map((restaurante) => {
+            return(
+            <>
+                <Grid
+                    align="center"
+                    key={restaurante.id}
                     >
-                        <RestaurantCard restaurante={restaurante} />
-                    </Grid>
-                </>
-            ))}
-        </Grid>
-    )
-
+                    <RestaurantCard restaurante={restaurante}/>
+                </Grid>
+            </>
+            );
+        });
+    const pageCount = Math.ceil(restaurantes.length / restaurantesPerPage);
+    const changePage = ({ selected }) => {setPageNumber(selected);};
+    
+    //PAGINACIÓN
+    return(
+    <Grid container>
+        {displayRestaurantes}
+        <ReactPaginate
+            previousLabel={"Previous"}
+            nextLabel={"Next"}
+            pageCount={pageCount}
+            onPageChange={changePage}
+            containerClassName={"paginationBttns"}
+            previousLinkClassName={"previousBttn"}
+            nextLinkClassName={"nextBttn"}
+            disabledClassName={"paginationDisabled"}
+            activeClassName={"paginationActive"}
+        />
+    </Grid>
+    ) 
 }
