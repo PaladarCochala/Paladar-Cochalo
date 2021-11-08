449444bb3114d6f2e8fb52d1bef14aca94cf45be
Allen Vargas <allenbfox@gmail.com>
2021-11-08 01:53:16 -0400

    Arregaldo Auth0

--- src/index.js
+++ src/index.js
@@ -19,8 +19,8 @@ const providerConfig = {
   domain: config.domain,
   clientId: config.clientId,
   ...(config.audience ? { audience: config.audience } : null),
-  redirectUri: window.location.origin,
-  onRedirectCallback,
+  redirectUri: window.location.origin+"/home",
+  onRedirectCallback
 };
 
 ReactDOM.render(
